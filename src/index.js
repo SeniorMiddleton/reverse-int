5ef7929706ec2950e90f94c54d67e266740dc150
SeniorMiddleton <kiselr647@gmail.com>
2023-08-02 22:28:54 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const reversed = parseInt(
+        Math.abs(n).toString().split("").reverse().join("")
+    );
+
+    if (n < 0) {
+        return reversed;
+    } else {
+        return reversed;
+    }
+};
